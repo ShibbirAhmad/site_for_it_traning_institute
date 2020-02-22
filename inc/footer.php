8767a02b8067a55ce0acd862666d12fa675b0cab
ShibbirAhmad <shibbirahmad920229@gmail.com>
2020-02-22 22:42:13 +0600

    header,content and footer section has divided

---
+++ inc/footer.php
@@ -0,0 +1,121 @@
+
+		<!-- Footer -->
+		<footer class="footer section">
+			<!-- Footer Top -->
+			<div class="footer-top overlay">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-4 col-md-6 col-12">  
+							<!-- About -->
+							<div class="single-widget about">
+								<h2>About Eduland</h2>
+								<ul class="list">
+									<li><i class="fa fa-phone"></i>Phone: +123 456-7890 </li>
+									<li><i class="fa fa-envelope"></i>Email: <a href="mailto:info@youremail.com">Info@youremail.com</a></li>
+									<li><i class="fa fa-map-o"></i>Address: 123, Jolfikar state, Us</li>
+								</ul>
+								<!-- Social -->
+								<ul class="social"> 
+									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+									<li class="active"><a href="#"><i class="fa fa-facebook"></i></a></li>
+									<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+									<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+									<li><a href="#"><i class="fa fa-youtube"></i></a></li>
+								</ul>
+								<!-- End Social -->
+							</div>
+							<!--/ End About -->
+						</div>
+						<div class="col-lg-4 col-md-6 col-12">
+							<!-- Useful Links -->
+							<div class="single-widget list">
+								<h2>Useful Links</h2>
+								<ul>
+									<li><i class="fa fa-angle-right"></i><a href="#">Home</a></li>
+									<li><i class="fa fa-angle-right"></i><a href="#">About Eduland</a></li>
+									<li><i class="fa fa-angle-right"></i><a href="#">Our Courses</a></li>
+									<li><i class="fa fa-angle-right"></i><a href="#">Upcoming Events</a></li>
+									<li><i class="fa fa-angle-right"></i><a href="#">Faq's</a></li>
+								</ul>
+							</div>
+							<!--/ End Useful Links -->
+						</div>
+						<div class="col-lg-4 col-md-6 col-12">
+							<!-- Useful Links -->
+							<div class="single-widget opening-times">
+								<h2>Opening Hourse</h2>
+								<ul class="list">
+									<li><span>Saturday - Sunday</span><div class="value">09am - 05pm</div></li>
+									<li><span>Monday - Twisday</span><div class="value">10AM - 02PM</div></li>
+									<li><span>Widnesday</span><div class="value">10AM - 12PM</div></li>
+									<li><span>Thursday</span><div class="value">08AM - 05PM</div></li>
+									<li><span>Friday</span><div class="value off">closed</div></li>
+								</ul>
+							</div>
+							<!--/ End Useful Links -->
+						</div>
+					</div>
+				</div>
+			</div>
+			<!--/ End Footer Top -->
+			<!-- Footer Bottom -->
+			<div class="footer-bottom">
+				<div class="container">
+					<div class="row">
+						<div class="col-12">
+							<!-- Copyright -->
+							<div class="copyright">
+								<p>© Copyright Eduland 2019. Design & Development by <a href="http://themelamp.com">www.themelamp.com</a>, Theme Release By <a href="http://codeglim.com">www.codeglim.com</a></p>
+							</div>
+							<!--/ End Copyright -->
+						</div>
+					</div>
+				</div>
+			</div>
+			<!--/ End Footer Bottom -->
+		</footer>
+		<!--/ End Footer -->
+		
+		<!-- Jquery JS-->
+        <script src="js/jquery.min.js"></script>
+        <script src="js/jquery-migrate.min.js"></script>
+		<!-- Colors JS-->
+        <script src="js/colors.js"></script>
+		<!-- Popper JS-->
+        <script src="js/popper.min.js"></script>
+		<!-- Bootstrap JS-->
+        <script src="js/bootstrap.min.js"></script>
+		<!-- Owl Carousel JS-->
+        <script src="js/owl.carousel.min.js"></script>
+		<!-- Jquery Steller JS -->
+		<script src="js/jquery.stellar.min.js"></script>
+		<!-- Final Countdown JS -->
+		<script src="js/finalcountdown.min.js"></script>
+		<!-- Fancy Box JS-->
+		<script src="js/facnybox.min.js"></script>
+		<!-- Magnific Popup JS-->
+		<script src="js/jquery.magnific-popup.min.js"></script>
+		<!-- Circle Progress JS -->
+		<script src="js/circle-progress.min.js"></script>
+		<!-- Nice Select JS -->
+		<script src="js/niceselect.js"></script>
+		<!-- Jquery Steller JS-->
+        <script src="js/jquery.stellar.min.js"></script>
+		<!-- Jquery Steller JS-->
+        <script src="js/cube-portfolio.min.js"></script>
+		<!-- Slick Nav JS-->
+        <script src="js/slicknav.min.js"></script>
+		<!-- Easing JS-->
+        <script src="js/easing.min.js"></script>
+		<!-- Waypoints JS-->
+        <script src="js/waypoints.min.js"></script>
+		<!-- Counter Up JS -->
+		<script src="js/jquery.counterup.min.js"></script>
+		<!-- Scroll Up JS-->
+        <script src="js/jquery.scrollUp.min.js"></script>
+		<!-- Gmaps JS-->
+		<script src="js/gmaps.min.js"></script>
+		<!-- Main JS-->
+        <script src="js/main.js"></script>
+    </body>
+</html>
---
+++ inc/header.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html class="no-js" lang="zxx">
+    <head>
+		<!-- Meta Tags -->
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="keywords" content="Site keywords here">
+		<meta name="description" content="">
+		<meta name='copyright' content=''>
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		
+		<!-- Title -->
+		<title>Eduland &minus; Education & Courses HTML5 Template</title>
+		
+		<!-- Favicon -->
+		<link rel="icon" type="image/png" href="images/favicon.png">
+		
+		<!-- Web Font -->
+		<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900" rel="stylesheet">
+		
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<!-- Font Awesome CSS -->
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+		<!-- Nice Select CSS -->
+        <link rel="stylesheet" href="css/niceselect.css">
+		<!-- Fancy Box CSS -->
+        <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+		<!-- Fancy Box CSS -->
+        <link rel="stylesheet" href="css/cube-portfolio.min.css">
+		<!-- Owl Carousel CSS -->
+        <link rel="stylesheet" href="css/owl.carousel.min.css">
+		<!-- Animate CSS -->
+        <link rel="stylesheet" href="css/animate.min.css">
+		<!-- Slick Nav CSS -->
+        <link rel="stylesheet" href="css/slicknav.min.css">
+        <!-- Magnific Popup -->
+		<link rel="stylesheet" href="css/magnific-popup.css">
+		
+		<!-- Eduland Stylesheet -->
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="css/responsive.css">
+		
+		<!-- Eduland Colors -->
+		<link rel="stylesheet" href="css/colors/color1.css">
+		
+    </head>
+    <body>
+	
+		<!-- Header -->
+		<header class="header">
+			<!-- Header Inner -->
+			<div class="header-inner overlay">
+				<div class="container">
+					
+					<div class="row">
+						<div class="col-lg-3 col-md-3 col-12">
+							<!-- Logo -->
+							<div class="logo">
+								<a href="index.php"><img src="images/logo.png" alt="#"></a>
+							</div>
+							<!--/ End Logo -->
+							<div class="mobile-menu"></div>
+						</div>
+						<div class="col-lg-9 col-md-9 col-12">
+							<div class="menu-bar">
+								<nav class="navbar navbar-default">
+									<div class="navbar-collapse">
+										<!-- Main Menu -->
+										<ul id="nav" class="nav menu navbar-nav">
+											<li class="active"><a href="index.php"><i class="fa fa-home"></i>Home</a></li>
+											<li><a href="#"><i class="fa fa-clone"></i>Pages</a> 
+												<ul class="dropdown">
+													<li><a href="teachers.php">Teachers</a></li>
+												</ul>
+											</li>
+											<li><a href="courses.php"><i class="fa fa-clone"></i>Our Courses</a></li>
+											<li><a href="events.php"><i class="fa fa-bullhorn"></i>Events</a></li>
+											<li><a href="contact.php"><i class="fa fa-address-book"></i>Contact</a> </li>
+										</ul>
+										<!-- End Main Menu -->
+									</div> 
+								</nav>
+								<!-- Search Area -->
+								<div class="search-area">
+									<a href="#header" class="icon"><i class="fa fa-search"></i></a>
+									<form class="search-form">
+										<input type="text" placeholder="ex: premium course" name="search">
+										<button value="search " type="submit"><i class="fa fa-search"></i></button>
+									</form>
+								</div>	
+								<!-- End Search Area-->
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!--/ End Header Inner -->
+		</header>
+		<!--/ End Header -->
+		
